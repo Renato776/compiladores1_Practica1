b0f31f6a69de110d757d803ffc74838939c370f4
Renato Flores <renatojosuefloresperez@gmail.com>
2019-06-06 11:12:43 -0600

    First commit

---
+++ src/practica1/Node.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+import java.util.*;
+/**
+ *
+ * @author renato
+ */
+public class Node {
+    public ArrayList primeros;
+    public ArrayList ultimos;
+    public int numero;
+    public boolean anulable;
+    public String contenido;
+    public String tipo;
+}
